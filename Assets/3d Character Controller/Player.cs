0dda0758e3af032d7346199056cd49da5afdd3d6
momintlh <77355191+momintlh@users.noreply.github.com>
2025-08-16 21:35:00 +0500

    movement with animations

--- Assets/3d Character Controller/Player.cs
+++ Assets/3d Character Controller/Player.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] CharacterController cc;
+    [SerializeField] Animator animator;
 
     [SerializeField] float speed = 15f;
 
     [SerializeField] Vector3 moveDirection;
+
+    [SerializeField]
+    private float rotationSpeed = 5;
 
     void Awake()
     {
@@ -20,8 +25,16 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        cc.Move(speed * Time.deltaTime * moveDirection);
+        animator.SetBool("isRunning", input.magnitude != 0);
+
+        if (input.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(input.normalized);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        cc.Move(speed * Time.deltaTime * input.normalized);
     }
 }
